20ad561707159eca601bc2bafa4a35cb0d5bf6f3
skx <610262374@qq.com>
2020-12-08 23:16:37 +0800

    -

--- SoEasyPlatform/Code/AppStart/InitTable.cs
+++ SoEasyPlatform/Code/AppStart/InitTable.cs
@@ -15,6 +15,119 @@ namespace SoEasyPlatform
             {
                 db.DbMaintenance.DropTable("Menu");
             }
+            InitMenu(db);
+
+            InitConnection(db);
+
+            InitTemplate(db);
+
+            InitNetVersion(db);
+
+            InitNuget(db);
+        }
+
+        private static void InitConnection(SqlSugarClient db)
+        {
+            db.CodeFirst.InitTables<DBConnection>();
+        }
+
+        private static void InitNuget(SqlSugarClient db)
+        {
+            db.CodeFirst.InitTables<Nuget>();
+            if (db.Queryable<Nuget>().Count() == 0)
+            {
+                db.Insertable(new List<Nuget>() {
+                     new Nuget(){
+                        NetVersion=2,
+                        Version="4.9.9.11",
+                        Name="sqlSugar"
+                     } }).ExecuteCommand();
+                db.Insertable(new List<Nuget>() {
+                     new Nuget(){
+                        NetVersion=3,
+                        Version="5.0.1.5",
+                        Name="sqlSugar"
+                     } }).ExecuteCommand();
+                db.Insertable(new List<Nuget>() {
+                     new Nuget(){
+                        NetVersion=21,
+                        Version="5.0.1.5",
+                        Name="sqlSugarCore"
+                     } }).ExecuteCommand();
+                db.Insertable(new List<Nuget>() {
+                     new Nuget(){
+                        NetVersion=31,
+                        Version="5.0.1.5",
+                        Name="sqlSugarCore"
+                     } }).ExecuteCommand();
+            }
+        }
+
+        private static void InitNetVersion(SqlSugarClient db)
+        {
+            db.CodeFirst.InitTables<NetVersion>();
+            if (db.Queryable<NetVersion>().Count() == 0)
+            {
+                db.Insertable(new List<NetVersion>() {
+                     new NetVersion(){
+                        Id=1,
+                        Name="不生成类库，不更新类库"
+                     },
+                     new NetVersion(){
+                        Id=2,
+                        Name=".NET Freamework 4.0 生成类库 ，更新类库"
+                     },
+                     new NetVersion(){
+                        Id=3,
+                        Name=".NET Freamework 4.5 生成类库 ，更新类库"
+                     },
+
+                    new NetVersion(){
+                        Id=21,
+                        Name=".NET Core 3.1 生成类库 ，更新类库"
+                     },
+
+                    new NetVersion(){
+                        Id=31,
+                        Name=".NET 5 生成类库 ，更新类库"
+                     }
+
+                }).ExecuteCommand();
+
+            }
+        }
+
+        private static void InitTemplate(SqlSugarClient db)
+        {
+            db.CodeFirst.InitTables<Template, TemplateType>();
+            if (db.Queryable<Template>().Count() == 0)
+            {
+                db.Insertable(new List<TemplateType>()
+                {
+                     new  TemplateType(){   Name="实体" },
+                     new  TemplateType(){   Name="业务" },
+                     new  TemplateType(){   Name="Web" }
+
+                }).ExecuteCommand();
+            }
+            if (db.Queryable<Template>().Count() == 0)
+            {
+                db.Insertable(new Template()
+                {
+                    ChangeTime = DateTime.Now,
+                    Content = RazorFirst.DefaultRazorClassTemplate,
+                    IsSystemData = true,
+                    TemplateTypeName = "实体",
+                    Sort = 0,
+                    TemplateTypeId = 1,
+                    Title = "SqlSugar默认实体模版"
+
+                }).ExecuteCommand();
+            }
+        }
+
+        private static void InitMenu(SqlSugarClient db)
+        {
             db.CodeFirst.InitTables<Menu>();
             db.Insertable(new List<Menu>()
                 {
@@ -50,94 +163,6 @@ namespace SoEasyPlatform
 
                 })
                 .AddSubList(it => it.Child.First().ParentId).ExecuteReturnPrimaryKey();
-
-            db.CodeFirst.InitTables<DBConnection>();
-            db.CodeFirst.InitTables<Template, TemplateType>();
-            if (db.Queryable<Template>().Count() == 0)
-            {
-                db.Insertable(new List<TemplateType>()
-                {
-                     new  TemplateType(){   Name="实体" },
-                     new  TemplateType(){   Name="业务" },
-                     new  TemplateType(){   Name="Web" }
-
-                }).ExecuteCommand();
-            }
-
-            if (db.Queryable<Template>().Count() == 0)
-            {
-                db.Insertable(new Template()
-                {
-                    ChangeTime = DateTime.Now,
-                    Content = RazorFirst.DefaultRazorClassTemplate,
-                    IsSystemData = true,
-                    TemplateTypeName = "实体",
-                    Sort = 0,
-                    TemplateTypeId = 1,
-                    Title = "SqlSugar默认实体模版"
-
-                }).ExecuteCommand();
-            }
-
-            db.CodeFirst.InitTables<NetVersion>();
-            if (db.Queryable<NetVersion>().Count() == 0) 
-            {
-                db.Insertable(new List<NetVersion>() { 
-                     new NetVersion(){ 
-                        Id=1,
-                        Name="不生成类库，不更新类库"
-                     },
-                     new NetVersion(){
-                        Id=2,
-                        Name=".NET Freamework 4.0 生成类库 ，更新类库"
-                     },
-                     new NetVersion(){
-                        Id=3,
-                        Name=".NET Freamework 4.5 生成类库 ，更新类库"
-                     },
-
-                    new NetVersion(){
-                        Id=21,
-                        Name=".NET Core 3.1 生成类库 ，更新类库"
-                     },
-
-                    new NetVersion(){
-                        Id=31,
-                        Name=".NET 5 生成类库 ，更新类库"
-                     }
-
-                }).ExecuteCommand();
-
-            }
-            db.DbMaintenance.DropTable("Nuget");
-            db.CodeFirst.InitTables<Nuget>();
-            if (db.Queryable<Nuget>().Count() == 0)
-            {
-                db.Insertable(new List<Nuget>() {
-                     new Nuget(){
-                        NetVersion=2,
-                        Version="4.9.9.11",
-                        Name="sqlSugar"
-                     } }).ExecuteCommand();
-                db.Insertable(new List<Nuget>() {
-                     new Nuget(){
-                        NetVersion=3,
-                        Version="5.0.1.5",
-                        Name="sqlSugar"
-                     } }).ExecuteCommand();
-                db.Insertable(new List<Nuget>() {
-                     new Nuget(){
-                        NetVersion=21,
-                        Version="5.0.1.5",
-                        Name="sqlSugarCore"
-                     } }).ExecuteCommand();
-                db.Insertable(new List<Nuget>() {
-                     new Nuget(){
-                        NetVersion=31,
-                        Version="5.0.1.5",
-                        Name="sqlSugarCore"
-                     } }).ExecuteCommand();
-            }
         }
     }
 }
