250d7dada968f4ce86e093feef99c75599768736
skx <610262374@qq.com>
2020-12-09 11:48:33 +0800

    -

---
+++ SoEasyPlatform/Pages/Shared/_Window.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SoEasyPlatform.Pages.Shared
+{
+    public class _WindowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
