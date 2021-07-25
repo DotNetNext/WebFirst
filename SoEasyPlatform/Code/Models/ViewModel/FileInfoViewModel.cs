7f49ae4ba35369ca3bf7d0525030563831e5db8d
sunkaixuna <610262374@qq.com>
2021-07-25 16:51:36 +0800

    -

--- SoEasyPlatform/Code/Models/ViewModel/FileInfoViewModel.cs
+++ SoEasyPlatform/Code/Models/ViewModel/FileInfoViewModel.cs
@@ -10,14 +10,16 @@ namespace SoEasyPlatform
         [PropertyName("编号")]
         public int? Id { get; set; }
         [ValidateReduired()]
-        [PropertyName("类库名")]
+        [PropertyName("名称")]
         public string Name { get; set; }
         [ValidateReduired()]
-        [PropertyName("类库版本")]
-        public string Version { get; set; }
+        [PropertyName("文件模版")]
+        public string Content { get; set; }
+        [PropertyName("填充模版")]
+        public string Json { get; set; }
         [ValidateReduired()]
-        [PropertyName(".Net版本")]
-        public int? NetVersion { get; set; }
+        [PropertyName("文件后缀")]
+        public string Suffix { get; set; }
     }
     public class FileInfoGridViewModel
     {
