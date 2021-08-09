5e09b595c57046a0386cb7462e96da0a68863153
sunkaixuna <610262374@qq.com>
2021-08-09 22:46:51 +0800

    -

--- SoEasyPlatform/wwwroot/build/js/custom.js
+++ SoEasyPlatform/wwwroot/build/js/custom.js
@@ -133,9 +133,7 @@ function init_sidebar() {
         });
         $SIDEBAR_MENU.find('a').filter(function () {
             return this.href == CURRENT_URL;
-        }).parent('li').addClass('current-page').parents('ul').slideDown(function () {
-            setContentHeight();
-        }).parent().addClass('active');
+        }).parent('li').addClass('current-page');
 
     
         // recompute content when resizing
