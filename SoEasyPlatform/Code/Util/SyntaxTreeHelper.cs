c506b14e365cb150780e1638b58eb3afc8a88df7
sunkaixuna <610262374@qq.com>
2021-08-12 00:16:08 +0800

    -

---
+++ SoEasyPlatform/Code/Util/SyntaxTreeHelper.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Threading.Tasks;
+
+namespace SoEasyPlatform 
+{
+    public class SyntaxTreeHelper
+    {
+        private static Type GetModelTypeByClass(string classString, string typeName)
+        {
+            //Write("Parsing the code into the SyntaxTree");
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(classString);
+
+            string assemblyName = Path.GetRandomFileName();
+            var refPaths = new[] {
+                //typeof(AccessViolationException).Assembly.Location,
+                typeof(System.Object).GetTypeInfo().Assembly.Location,
+                typeof(Console).GetTypeInfo().Assembly.Location,
+                Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "System.Runtime.dll"),
+                typeof(SqlSugar.SqlSugarClient).Assembly.Location
+            };
+            MetadataReference[] references = refPaths.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
+
+
+            //Write("Adding the following references");
+            //foreach (var r in refPaths)
+            // Write(r);
+
+            //Write("Compiling ...");
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    //Write("Compilation failed!");
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                        diagnostic.IsWarningAsError ||
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("\t{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                    throw new Exception("解析出错");
+                }
+                else
+                {
+                    //Write("Compilation successful! Now instantiating and executing the code ...");
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+                    var type = assembly.GetType("RoslynCompileSample." + typeName);
+                    //Console.WriteLine(type.Name);
+                    return type;
+                }
+            }
+        }
+    }
+}
