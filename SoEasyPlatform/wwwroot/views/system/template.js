1dda69dbb4525ec639a52f56d6de7a4199248d15
sunkaixuna <610262374@qq.com>
2021-08-15 16:05:46 +0800

    添加模版说明

--- SoEasyPlatform/wwwroot/views/system/template.js
+++ SoEasyPlatform/wwwroot/views/system/template.js
@@ -72,7 +72,11 @@ btnAdd.$Open("#divOpen", {
     },
     btn: ['添加', '关闭']
 });
-
+btnParameter.$Open("#divRemark", {
+    title:"参数说明",
+    w: 550,
+    h: configs.w.h,
+})
 btnEdit.$Open("#divOpen", {
     title: configs.text.edit,
     w: configs.w.w,
