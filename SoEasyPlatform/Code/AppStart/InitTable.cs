b675360597f91436a9bc76e65872a5feb23c6a83
sunkaixuna <610262374@qq.com>
2021-09-18 21:49:03 +0800

    -

--- SoEasyPlatform/Code/AppStart/InitTable.cs
+++ SoEasyPlatform/Code/AppStart/InitTable.cs
@@ -480,9 +480,9 @@ namespace SoEasyPlatform
                         {
                              new Menu{ MenuName="WebFirst",Url="https://www.donet5.com/Doc/11?src=webfirst" },
                              new Menu{ MenuName="SqlSugar ORM" ,Url="https://www.donet5.com/Home/Doc?src=webfirst" },
-                             new Menu{ MenuName="Sugar.IOC" ,Url="https://www.donet5.com/Doc/10/2250" },
-                             new Menu{ MenuName="打赏" ,Url="https://www.donet5.com/Doc/28/2357" },
-                             new Menu{ MenuName="商务合作" ,Url="https://www.donet5.com/Doc/28/2358" },
+                             new Menu{ MenuName="Sugar.IOC" ,Url="https://www.donet5.com/Doc/10/2250?src=webfirst" },
+                             new Menu{ MenuName="打赏" ,Url="https://www.donet5.com/Doc/28/2357?src=webfirst" },
+                             new Menu{ MenuName="商务合作" ,Url="https://www.donet5.com/Doc/28/2358?src=webfirst" },
                         }
                      }
 
