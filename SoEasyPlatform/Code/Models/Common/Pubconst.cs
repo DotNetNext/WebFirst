8b968e1166156c9abecfe6692e4500ba19e40d39
sunkaixuna <610262374@qq.com>
2021-09-18 21:14:24 +0800

    -

--- SoEasyPlatform/Code/Models/Common/Pubconst.cs
+++ SoEasyPlatform/Code/Models/Common/Pubconst.cs
@@ -13,5 +13,9 @@ namespace SoEasyPlatform
 
         public const string MESSAGEADDSUCCESS = "添加成功";
         public const string MESSAGESAVESUCCESS = "保存成功";
+
+
+
+        public const string DEFAULTSOLUTIUON = "93DE3D8F-3D83-4D46-8C6A-D9A79400C850";
     }
 }
