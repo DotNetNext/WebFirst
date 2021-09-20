ace69f3dc6c10401f5256a06c01f5cb14d2531d8
sunkaixuna <610262374@qq.com>
2021-09-20 20:22:04 +0800

    -

--- SoEasyPlatform/Code/AppStart/InitTable.cs
+++ SoEasyPlatform/Code/AppStart/InitTable.cs
@@ -496,8 +496,8 @@ namespace SoEasyPlatform
                         Child=new List<Menu>()
                         {
                              new Menu{ MenuName="配置数据库" , Url="/Database"},
-                             new Menu{ MenuName="配置实体-类建表" , Url="/CodeFirst"},
-                             new Menu{ MenuName="配置实体-表建类" , Url="/DbFirst"},
+                             new Menu{ MenuName="配置实体-类建表模式" , Url="/CodeFirst"},
+                             new Menu{ MenuName="配置实体-表建类模式" , Url="/DbFirst"},
                              new Menu{ MenuName="配置业务",Url="/BIZ" },
                              new Menu{ MenuName="配置前端" ,Url="/Web"},
                              new Menu{ MenuName="方案管理" , Url="/Project"} ,
