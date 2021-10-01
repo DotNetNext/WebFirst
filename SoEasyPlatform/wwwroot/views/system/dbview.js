a6a739d61465f415814ba67f3d7e39ba322b80bd
sunkaixuna <610262374@qq.com>
2021-10-01 15:05:26 +0800

    -

--- SoEasyPlatform/wwwroot/views/system/dbview.js
+++ SoEasyPlatform/wwwroot/views/system/dbview.js
@@ -6,7 +6,7 @@ btnDel2.onclick = function () {
     btnDel.click();
 }
 btnAddView.$Open("#divView", {
-    title: configs.text.copy,
+    title: "创建类",
     w: 600,
     h: 300,
     validate: function () {
@@ -14,34 +14,31 @@ btnAddView.$Open("#divView", {
         if (txtDbId.value == null || txtDbId.value == "" || txtDbId.value == "0") {
             "请选择数据库".$Alert();
             return false;
-        } else if (gridInfo.length == 0) {
-            "请选择记录".$Alert();
-            return false;
         } else {
             return true;
         }
     },
     yes: function () {
-        var gridInfo = divGrid.$GridInfo();
-        if (gridInfo.length > 0) {
-            SaveTable2.value = JSON.stringify(gridInfo);
-            btnProject.$Loading();
-            frmProjectSave.$Form({
-                url: configs.url.Copy,
-                callback: function (msg) {
-                    btnProject.$CloseLoading();
-                    if (msg.IsSuccess) {
-                        $sugar.$CloseAll(divProject.getAttribute("dataindex"));
-                        btnCopyHide.click();
-                    }
-                    else {
-                        msg.Data.$Alert();
-                    }
-                }
-            });
-        } else {
-            "请选择一条数据".$Alert();
-        }
+        //var gridInfo = divGrid.$GridInfo();
+        //if (gridInfo.length > 0) {
+        //    SaveTable2.value = JSON.stringify(gridInfo);
+        //    btnProject.$Loading();
+        //    frmProjectSave.$Form({
+        //        url: configs.url.Copy,
+        //        callback: function (msg) {
+        //            btnProject.$CloseLoading();
+        //            if (msg.IsSuccess) {
+        //                $sugar.$CloseAll(divProject.getAttribute("dataindex"));
+        //                btnCopyHide.click();
+        //            }
+        //            else {
+        //                msg.Data.$Alert();
+        //            }
+        //        }
+        //    });
+        //} else {
+        //    "请选择一条数据".$Alert();
+        //}
     },
     btn: ['创建类', '关闭']
 });
