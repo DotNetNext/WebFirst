12017e88796c86a3232a88be678ef124431e09e0
sunkaixuna <610262374@qq.com>
2021-10-01 14:58:29 +0800

    -

--- SoEasyPlatform/wwwroot/views/system/dbview.js
+++ SoEasyPlatform/wwwroot/views/system/dbview.js
@@ -5,3 +5,43 @@
 btnDel2.onclick = function () {
     btnDel.click();
 }
+btnAddView.$Open("#divView", {
+    title: configs.text.copy,
+    w: 600,
+    h: 300,
+    validate: function () {
+        var gridInfo = divGrid.$GridInfo();
+        if (txtDbId.value == null || txtDbId.value == "" || txtDbId.value == "0") {
+            "请选择数据库".$Alert();
+            return false;
+        } else if (gridInfo.length == 0) {
+            "请选择记录".$Alert();
+            return false;
+        } else {
+            return true;
+        }
+    },
+    yes: function () {
+        var gridInfo = divGrid.$GridInfo();
+        if (gridInfo.length > 0) {
+            SaveTable2.value = JSON.stringify(gridInfo);
+            btnProject.$Loading();
+            frmProjectSave.$Form({
+                url: configs.url.Copy,
+                callback: function (msg) {
+                    btnProject.$CloseLoading();
+                    if (msg.IsSuccess) {
+                        $sugar.$CloseAll(divProject.getAttribute("dataindex"));
+                        btnCopyHide.click();
+                    }
+                    else {
+                        msg.Data.$Alert();
+                    }
+                }
+            });
+        } else {
+            "请选择一条数据".$Alert();
+        }
+    },
+    btn: ['创建类', '关闭']
+});
