0dd54bfff19bb87a12a3b636130f68695f7c826d
sunkaixuna <610262374@qq.com>
2021-10-02 17:30:51 +0800

    -

--- SoEasyPlatform/wwwroot/views/system/dbview.js
+++ SoEasyPlatform/wwwroot/views/system/dbview.js
@@ -26,6 +26,7 @@ btnAddView.$Open("#divView", {
             callback: function (msg) {
                 btnProject.$CloseLoading();
                 if (msg.IsSuccess) {
+                    ClassName.value = viewclassname.value;
                     $sugar.$CloseAll(divView.getAttribute("dataindex"));
                     btnSearch.click();
                 }
