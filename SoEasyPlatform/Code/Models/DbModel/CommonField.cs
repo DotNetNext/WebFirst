6ef2f3f6885239ca01a35a8d3db01745df0140b8
sunkaixuna <610262374@qq.com>
2021-10-02 20:29:21 +0800

    配置公共字段

---
+++ SoEasyPlatform/Code/Models/DbModel/CommonField.cs
@@ -0,0 +1,23 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoEasyPlatform 
+{
+    public class CommonField
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int Id { get; set; }
+        public string ClassProperName { get; set; }
+        public string DbColumnName { get; set; }
+        public bool Required { get; set; }
+        public bool IsIdentity { get; set; }
+        public bool IsPrimaryKey { get; set; }
+        [SugarColumn(IsNullable = true)]
+        public string Description { get; set; }
+        public string CodeType { get; set; }
+        public int CodeTableId { get; set; }
+    }
+}
