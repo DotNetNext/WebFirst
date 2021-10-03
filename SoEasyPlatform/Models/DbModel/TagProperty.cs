c86167632018fab1aaac6dea6d2485dcd3d1dce0
sunkaixuna <610262374@qq.com>
2021-10-03 22:35:06 +0800

    -

--- SoEasyPlatform/Models/DbModel/TagProperty.cs
+++ SoEasyPlatform/Models/DbModel/TagProperty.cs
@@ -5,7 +5,24 @@ using System.Threading.Tasks;
 
 namespace SoEasyPlatform 
 {
+    /// <summary>
+    /// 属性标签，用于模版扩展
+    /// </summary>
     public class TagProperty
     {
+        [SqlSugar.SugarColumn(IsPrimaryKey =true,IsIdentity =true)]
+        public int Id { get; set; }
+        public string UniueCode { get; set; }
+        public string Description { get; set; }
+        public string ControlType { get; set; }
+        public string UrlKey { get; set; }
+        public string Url { get; set; }
+        public string FileValue { get; set; }
+        public string FileName { get; set; }
+        public string Ext1 { get; set; }
+        public string Ext2 { get; set; }
+        public string Ext3 { get; set; }
+        public string Ext4 { get; set; }
+        public string Ext5 { get; set; }
     }
 }
