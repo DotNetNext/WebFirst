81993b1e1775f57734fd3d225bd5fb43692d2e71
sunkaixuna <610262374@qq.com>
2021-10-03 10:28:44 +0800

    -

--- SoEasyPlatform/Startup.cs
+++ SoEasyPlatform/Startup.cs
@@ -24,7 +24,7 @@ namespace SoEasyPlatform
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Version = "12311131";
+        public static string Version = "123111131";
         /// <summary>
         /// 接口域名目录
         /// </summary>
---
+++ SoEasyPlatform/wwwroot/views/system/commonfield.js
@@ -0,0 +1,141 @@
+﻿var configs = {
+    url: {
+        Get: _root + "CommonFiled/GetCommonFiledList",
+        Del: _root + "CommonFiled/DeleteCommonFiled",
+        SaveSystem: _root + "CommonFiled/SaveCommonFiled",
+        GetDbType: _root +"CommonFiled/GetCommonFiledType"
+    },
+    text:
+    {
+        add: "添加模版",
+        edit:"修改模版"
+    },
+    w: {
+        w: 1200,
+        h:570
+    }
+};
+divFrom.$Form({
+    url: configs.url.Get,
+    callback: function (msg) {
+        msg.Data.Dblfunc = function () {
+            btnEdit.click();
+        };
+        divGrid.$Grid(msg.Data);
+    }
+})
+btnSearch.$Button({
+    url: configs.url.Get,
+    callback: function (msg) {
+        msg.Data.Dblfunc = function () {
+            btnEdit.click();
+        };
+        divGrid.$Grid(msg.Data);
+    }
+});
+
+
+saveTemplateTypeName.$SelectTree({
+    isMultiple: false,
+    url: configs.url.GetDbType,
+    maxHeight: 180,
+    rootIsSelect: false
+})
+
+btnReset.$Reset();
+
+
+btnAdd.$Open("#divOpen", {
+    title: configs.text.add,
+    w: configs.w.w,
+    h: configs.w.h,
+    validate: function () {
+        frmSave.$ClearControls();
+        return true;
+    },
+    yes: function () {
+        frmSave.$Form({
+            url: configs.url.SaveSystem,
+            callback: function (msg) {
+                if (msg.IsKeyValuePair) {
+                    $sugar.$Validate(msg.Data, "save");
+                } else {
+                    $sugar.$Validate("clear");
+                    msg.Data.$Alert();
+                    if (msg.IsSuccess) {
+                        btnSearch.click();
+                        $sugar.$CloseAll(divOpen.getAttribute("dataindex"));
+                    }
+                }
+            }
+        });
+    },
+    btn: ['添加', '关闭']
+});
+btnParameter.$Open("#divRemark", {
+    title:"参数说明",
+    w: 550,
+    h: configs.w.h,
+})
+btnEdit.$Open("#divOpen", {
+    title: configs.text.edit,
+    w: configs.w.w,
+    h: configs.w.h,
+    validate: function () {
+        var gridInfo = divGrid.$GridInfo();
+        if (gridInfo.length == 0) {
+            "请选择记录".$Alert();
+            return false;
+        } else {
+            gridInfo = gridInfo[0];
+            frmSave.$FillControls(gridInfo);
+            return true;
+        }
+    },
+    yes: function () {
+        frmSave.$Form({
+            url: configs.url.SaveSystem,
+            callback: function (msg) {
+                if (msg.IsKeyValuePair) {
+                    $sugar.$Validate(msg.Data, "save");
+                } else {
+                    $sugar.$Validate("clear");
+                    msg.Data.$Alert();
+                    if (msg.IsSuccess) {
+                        btnSearch.click();
+                        $sugar.$CloseAll(divOpen.getAttribute("dataindex"));
+                    }
+                }
+            }
+        });
+    },
+    btn: ['保存', '关闭']
+});
+
+
+btnDel.$Confirm({
+    title: "是否删除记录",
+    ok: function () {
+        var gridInfo = divGrid.$GridInfo();
+        if (gridInfo.length > 0) {
+            configs.url.Del.$Ajax({
+                callback: function (msg) {
+                    if (msg.IsSuccess) {
+                        "删除成功".$Alert();
+                        btnSearch.click();
+                    }
+                    else
+                    {
+                        msg.Data.$Alert();
+                    }
+                },
+                data: { "model": JSON.stringify(gridInfo) }
+            })
+        } else {
+            "请选择一条数据".$Alert();
+        }
+    }
+})
+
+
+ 
