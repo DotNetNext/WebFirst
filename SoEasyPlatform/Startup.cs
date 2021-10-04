222df481d8e4c5fad0dbd060c7e411bb02513b6e
sunkaixuna <610262374@qq.com>
2021-10-05 01:20:00 +0800

    -

--- SoEasyPlatform/Startup.cs
+++ SoEasyPlatform/Startup.cs
@@ -24,7 +24,7 @@ namespace SoEasyPlatform
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Version = "24522224";
+        public static string Version = "2452222224";
         /// <summary>
         /// 接口域名目录
         /// </summary>
