cd84d8e1d7a6fb96ad87eeba5d952e4acd6f0486
sunkaixuna <610262374@qq.com>
2021-10-21 20:57:58 +0800

    修改样式

--- SoEasyPlatform/Startup.cs
+++ SoEasyPlatform/Startup.cs
@@ -24,7 +24,7 @@ namespace SoEasyPlatform
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Version = "1.26";
+        public static string Version = "1.261";
         /// <summary>
         /// 接口域名目录
         /// </summary>
--- SoEasyPlatform/wwwroot/views/shared/layout.js
+++ SoEasyPlatform/wwwroot/views/shared/layout.js
@@ -1,4 +1,8 @@
 ﻿divMenu.$Fill({ SugarUrl: _root + "System/getmenu" })
 iframeBody.$AddCss({ "height": $sugar.$WindowHeight() - 75 })
+window.onresize = function ()
+{
+    iframeBody.$AddCss({ "height": $sugar.$WindowHeight() - 75 })
+}
 
  
