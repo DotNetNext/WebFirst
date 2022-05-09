c8cf657e34ff04f2e0eb169289bd66d159822d92
sunkaixuan <610262374@qq.com>
2022-05-09 12:50:57 +0800

    修改描述

--- SoEasyPlatform/Startup.cs
+++ SoEasyPlatform/Startup.cs
@@ -24,7 +24,7 @@ namespace SoEasyPlatform
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Version = "1.36";
+        public static string Version = "1.361";
         /// <summary>
         /// 接口域名目录
         /// </summary>
