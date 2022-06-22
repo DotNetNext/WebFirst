2345c5a8132ab3c36bc8f7100c37ede23f114753
sunkaixuan <610262374@qq.com>
2022-06-22 15:36:48 +0800

    -

--- SoEasyPlatform/InitProjects.cs
+++ SoEasyPlatform/InitProjects.cs
@@ -1,5 +1,6 @@
 ﻿using SqlSugar;
 using System.IO;
+using System.Xml.Linq;
 
 namespace SoEasyPlatform
 {
@@ -12,8 +13,25 @@ namespace SoEasyPlatform
         {
             var directory = FileSugar.MergeUrl(Directory.GetCurrentDirectory(), "wwwroot", "template", "Projects");
             var projectPathList=Directory.GetDirectories(directory);
-            var configUrl = directory + "Config.json";
-            var jsonConfig = "directory";
+            foreach (var project in projectPathList)
+            {
+                var configUrl = FileSugar.MergeUrl(project, "Config.json");
+                CheckProjectConfig(configUrl);
+                var json = XElement.Parse(configUrl);
+            }
+        }
+
+        /// <summary>
+        /// 验证是否存在项目配置
+        /// </summary>
+        /// <param name="configUrl"></param>
+        /// <exception cref="System.Exception"></exception>
+        private static void CheckProjectConfig(string configUrl)
+        {
+            if (FileSugar.IsExistFile(configUrl) == false)
+            {
+                throw new System.Exception("没有找到文件" + configUrl);
+            }
         }
     }
 }
