cab950ed8662f58eded4392315a457df77d27cec
sunkaixuan <610262374@qq.com>
2022-06-24 05:35:13 +0800

    添加模版

---
+++ SoEasyPlatform/wwwroot/template/Projects/FurionSqlSugarMvc/FurionMvc.Application/System/SystemService.cs
@@ -0,0 +1,10 @@
+﻿namespace FurionMvc.Application
+{
+    public class SystemService : ISystemService, ITransient
+    {
+        public string GetDescription()
+        {
+            return "让 .NET 开发更简单，更通用，更流行。";
+        }
+    }
+}
