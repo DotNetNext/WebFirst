4e5174cb47918df2cc2e75b5493a127d4b254810
sunkaixuan <610262374@qq.com>
2022-06-23 17:39:08 +0800

    还原到.NET CORE 3.1

--- SoEasyPlatform/Program.cs
+++ SoEasyPlatform/Program.cs
@@ -1,26 +1,26 @@
-using Microsoft.AspNetCore.Builder;
-using SoEasyPlatform;
-using SqlSugar.IOC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-var builder = WebApplication.CreateBuilder(args);
-Services.AddServices(builder.Services);
-builder.Services.AddSqlSugar(new SqlSugar.IOC.IocConfig()
+namespace SoEasyPlatform
 {
-    ConfigId = "master1",
-    DbType = IocDbType.Sqlite,
-    IsAutoCloseConnection = true,
-    ConnectionString = "DataSource=" + Startup.CurrentDirectory + @"\database\sqlite.db"
-});
-builder.Services.ConfigurationSugar(db =>
-{
-    if (!db.ConfigQuery.Any())
+    public class Program
     {
-        db.ConfigQuery.SetTable<Template>(it => it.Id, it => it.Title);
-        db.ConfigQuery.SetTable<TemplateType>(it => it.Id, it => it.Name);
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
-});
-
-var app = builder.Build();
-Configures.AddConfigure(app, app.Environment);
-
-app.Run();
+}
--- SoEasyPlatform/Startup.cs
+++ SoEasyPlatform/Startup.cs
@@ -1,4 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SqlSugar.IOC;
 namespace SoEasyPlatform
 {
     /// <summary>
@@ -16,12 +30,56 @@ namespace SoEasyPlatform
         /// </summary>
         /// <param name="configuration"></param>
         public static string RootUrl = "/api/";
+        #endregion
 
+        #region 配置方法
         /// <summary>
-        /// CurrentDirectory
+        /// 构造函数
         /// </summary>
-        public static string CurrentDirectory = Environment.CurrentDirectory;
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        /// <summary>
+        /// 配置对象
+        /// </summary>
+        public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Services.AddServices(services);
+            services.AddSqlSugar(new SqlSugar.IOC.IocConfig()
+            {
+                ConfigId = "master1",
+                DbType = IocDbType.Sqlite,
+                IsAutoCloseConnection = true,
+                ConnectionString = "DataSource=" + GetCurrentDirectory() + @"\database\sqlite.db"
+            });
+            services.ConfigurationSugar(db =>
+            {
+                if (!db.ConfigQuery.Any())
+                {
+                    db.ConfigQuery.SetTable<Template>(it => it.Id, it => it.Title);
+                    db.ConfigQuery.SetTable<TemplateType>(it => it.Id, it => it.Name);
+                }
+            });
+        }
+
+        public static string GetCurrentDirectory()
+        {
+#if DEBUG
+            return AppContext.BaseDirectory;
+#endif
+            return System.Environment.CurrentDirectory;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            Configures.AddConfigure(app, env);
+        }
         #endregion
     }
 }
