c95c37ce49db03f69b6be692ad7b80998c0c5a03
sunkaixuan <610262374@qq.com>
2023-03-26 19:31:29 +0800

    1.5.5

--- SoEasyPlatform/Startup.cs
+++ SoEasyPlatform/Startup.cs
@@ -25,7 +25,7 @@ namespace SoEasyPlatform
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Version = "1.5.42";
+        public static string Version = "1.5.5";
         /// <summary>
         /// 接口域名目录
         /// </summary>
